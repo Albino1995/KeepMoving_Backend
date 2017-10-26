66bc7c6d1eb2f9bb5beefd9088b1d86dcc0e89c4
Albino1995 <763968024@qq.com>
2017-10-26 10:28:17 +0800

    Update Model

--- KeepMoving_Backend/settings.py
+++ KeepMoving_Backend/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0, BASE_DIR)
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -25,8 +28,10 @@ SECRET_KEY = '^9@+g^2i9a)9%(*n^8j7$oxzoothn9f7u=!iw-cn9tkn1+1&z+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+# 替换系统用户
+AUTH_USER_MODEL = 'users.UserProfile'
 
 # Application definition
 
@@ -36,7 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
+    'users.apps.UsersConfig',
+    'goods.apps.GoodsConfig',
+    'trade.apps.TradeConfig',
+    'user_operation.apps.UserOperationConfig',
+    'DjangoUeditor',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +85,13 @@ WSGI_APPLICATION = 'KeepMoving_Backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'keepmoving',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'',
+        # 使用INNODB引擎
+        'OPTIONS': {'init_command': 'SET default_storage_engine=INNODB;'}
     }
 }
 
@@ -104,18 +118,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'zh-hans'
+TIME_ZONE = 'Asia/Shanghai'
 USE_I18N = True
-
 USE_L10N = True
-
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
---
+++ apps/goods/models.py
@@ -0,0 +1,89 @@
+from datetime import datetime
+
+from django.db import models
+from DjangoUeditor.models import UEditorField
+# Create your models here.
+
+
+class Goods(models.Model):
+    """
+    商品
+    """
+    category = models.CharField(default="踩的", max_length=30,
+                                choices=(("踩的", "踩的"), ("穿的", "穿的"), ("戴的", "戴的")), verbose_name="类别", help_text="类别")
+    goods_sn = models.CharField(max_length=50, default="", verbose_name="商品货号")
+    name = models.CharField(max_length=100, verbose_name="商品名")
+    sold_num = models.IntegerField(default=0, verbose_name="商品销售量")
+    fav_num = models.IntegerField(default=0, verbose_name="收藏数")
+    price = models.FloatField(default=0, verbose_name="价格")
+    gender = models.CharField(max_length=6, choices=(("male", "男的"), ("female", "女的"), ("neutral", "男女同款")),
+                              default="female", verbose_name="性别")
+    goods_desc = UEditorField(verbose_name="内容", width=800, height=300, default='')
+    goods_front_image = models.ImageField(upload_to="goods/images/", null=True, blank=True, verbose_name="封面图")
+    is_new = models.BooleanField(default=False, verbose_name="是否新品")
+    is_hot = models.BooleanField(default=False, verbose_name="是否热销")
+    is_sale = models.BooleanField(default=False, verbose_name="是否优惠")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = '商品'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class GoodCS(models.Model):
+    """
+    商品色码
+    """
+    goods = models.ForeignKey(Goods, verbose_name="商品", related_name="cs")
+    goods_size = models.CharField(max_length=5, choices=(("35", "35"), ("36", "36"), ("37", "37"),
+                                                         ("38", "38"), ("39", "39"), ("40", "40"),
+                                                         ("41", "41"), ("42", "42"), ("43", "43"),
+                                                         ("44", "44"), ("45", "45"), ("S", "S"),
+                                                         ("M", "M"), ("L", "L"), ("XL", "XL"),
+                                                         ("均码", "均码")),
+                                  verbose_name="尺码")
+    goods_color = models.CharField(max_length=10, null=True, blank=True, verbose_name="颜色")
+    goods_num = models.IntegerField(default=0, verbose_name="库存数")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = '商品色码'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.goods.name
+
+
+class GoodImage(models.Model):
+    """
+    商品详情图
+    """
+    cs = models.ForeignKey(GoodCS, verbose_name="商品色码", related_name="img")
+    image = models.ImageField(upload_to="goods/images/", verbose_name="图片", null=True, blank=True)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = '商品图片'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.cs.goods.name
+
+
+class Banner(models.Model):
+    """
+    轮播图
+    """
+    image = models.ImageField(upload_to='banner', verbose_name="轮播图片")
+    index = models.IntegerField(default=0, verbose_name="轮播顺序")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = '轮播图'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.image
